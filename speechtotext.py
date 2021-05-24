f4cc288ce7a043ec61cb6b546f37d4c849fa7a38
arunimasundar <arunimasundar7@gmail.com>
2021-05-24 21:43:36 +0530

    modified speech

--- speechtotext.py
+++ speechtotext.py
@@ -10,30 +10,45 @@ def stt(videoname):
 
     video1 = mp.VideoFileClip(videoname)
     video_duration = int(video1.duration)
-  
-    # calculate dusration of the video
-  
-    # 52 minutes
-    num_seconds_video= video_duration
-    print("The video is {} seconds".format(num_seconds_video))
-    l=list(range(0,num_seconds_video+1,60))
-    # print(l)
 
-    diz={}
-    for i in range(len(l)-1):
-        ffmpeg_extract_subclip(videoname, l[i], l[i+1], targetname="chunks/cut{}.mp4".format(i+1))
-        clip = mp.VideoFileClip(r"chunks/cut{}.mp4".format(i+1)) 
-        clip.audio.write_audiofile(r"converted/converted{}.wav".format(i+1))
+    if video_duration<60:
+        
+        video1.audio.write_audiofile(r"converted.wav")
         r = sr.Recognizer()
-        audio = sr.AudioFile("converted/converted{}.wav".format(i+1))
+        audio = sr.AudioFile("converted.wav")
         with audio as source:
-            r.adjust_for_ambient_noise(source)  
             audio_file = r.record(source)
-            result = r.recognize_google(audio_file)
-            diz['chunk{}'.format(i+1)]=result
+        text = r.recognize_google(audio_file)
+    else:
 
-    l_chunks=[diz['chunk{}'.format(i+1)] for i in range(len(diz))]
-    text='\n'.join(l_chunks)
+
+
+    
+        # calculate dusration of the video
+    
+        # 52 minutes
+        num_seconds_video= video_duration
+        print("The video is {} seconds".format(num_seconds_video))
+        l=list(range(0,num_seconds_video+1,60))
+        # print(l)
+
+        diz={}
+        for i in range(len(l)-1):
+            ffmpeg_extract_subclip(videoname, l[i], l[i+1], targetname="chunks/cut{}.mp4".format(i+1))
+            clip = mp.VideoFileClip(r"chunks/cut{}.mp4".format(i+1)) 
+            clip.audio.write_audiofile(r"converted/converted{}.wav".format(i+1))
+            r = sr.Recognizer()
+            audio = sr.AudioFile("converted/converted{}.wav".format(i+1))
+            with audio as source:
+                r.adjust_for_ambient_noise(source)  
+                audio_file = r.record(source)
+                result = r.recognize_google(audio_file)
+                diz['chunk{}'.format(i+1)]=result
+
+        l_chunks=[diz['chunk{}'.format(i+1)] for i in range(len(diz))]
+        text='\n'.join(l_chunks)
+
+
     import requests
     url = 'http://bark.phon.ioc.ee/punctuator'
     texttest = text
